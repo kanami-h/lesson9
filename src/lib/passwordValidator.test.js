31c4f0c333997140b9c81242b8cd1c137be60ba2
Kanami <kanami.h217@gmail.com>
2019-09-11 16:31:32 +0900

    initial commit

---
+++ src/lib/passwordValidator.test.js
@@ -0,0 +1,8 @@
+import PasswordValidator from './passwordValidator';
+
+describe('password validator', () => {
+    it('妥当な場合は成功を返す', () => {});
+    it('空の場合は失敗を返す', () => {});
+    it('短すぎる場合は失敗を返す', () => {});
+    it('アルファベットの大文字、小文字、数字のいずれかがなければ失敗を返す', () => {});
+});
